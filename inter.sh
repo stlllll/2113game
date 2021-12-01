99e7ff319b48bcdc15c4d63e1c689d6d2c3e88a8
Sha Taole <h3577089@academy21.cs.hku.hk>
2021-12-01 14:05:17 +0800

    ,

--- inter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Do you want to restart or continue the last game? (restart/continue)"
-read choice
-if [ "$choice" == "restart" ]
-then
-    rm memory.txt
-    touch memory.txt
-fi
-./main
