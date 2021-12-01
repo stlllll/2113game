781cb7fb71fa764c5d112b166b6153543a019c76
Sha Taole <h3577089@academy21.cs.hku.hk>
2021-12-01 13:52:24 +0800

    Read from files

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 //This is the main program
 #include <iostream>
+#include <string>
 using namespace std;
+struct student{
+   int round;
+   int session;
+   string background;
+   string targets[4];
+   int time_distri[5];
+   double ability[5];
+};
+
 int main(){
+
 return 0;
 }
 
